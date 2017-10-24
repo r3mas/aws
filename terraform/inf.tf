04163dcd4355d38963fc6f81fc768b6d77e36cd5
remigiusz.glowacki <remigiusz.glowacki@yoti.com>
2017-10-24 23:04:55 +0100

    VPC adn AMI

--- terraform/inf.tf
+++ terraform/inf.tf
@@ -3,12 +3,38 @@ provider "aws" {
   profile = "${var.aws_profile}"
 }
 
-resource "aws_instance" "AnsibleMaster" {
+resource "aws_instance" "SaltMaster" {
   ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
   key_name      = "remi"
 
   tags {
-    Name = "AnsibleMaster"
+    Name = "SaltMaster"
   }
 }
+
+resource "aws_security_group" "consul_uld" {
+  name        = "consul_uld"
+  description = "Allow all traffic in consul network"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "consul_uld"
+  }
+}
+
+resource "aws_vpc" "consul_uld" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags {
+    Name = "consul_uld"
+  }
+}
+
